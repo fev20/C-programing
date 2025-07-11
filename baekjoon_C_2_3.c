90bdb4b4eba4931cc643fb48633aecbc3c452830
fev20 <isemy0317@gmail.com>
2025-07-11 17:18:48 +0900

    Update baekjoon_C_2_3.c

--- baekjoon_C_2_3.c
+++ baekjoon_C_2_3.c
@@ -11,3 +11,41 @@ int main(){
     }
     return 0;
 }
+
+
+// #include <stdio.h>
+// int main(){
+//     int a;
+//     scanf("%d", &a);
+//     if (a%4==0){
+//         if (a%100!=0){
+//             printf("1");
+//         }
+//         else{
+//             if (a%400==0){
+//                 printf("1");
+//             }
+//             else{
+//                 printf("0");
+//             }
+//         }
+//     }
+//     else{
+//         printf("0");
+//     }
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// int main(){
+//     int a;
+//     scanf("%d", &a);
+//     if (a%4==0 && a%100!=0 || a%400==0){
+//         printf("1");
+//     }
+//     else{
+//         printf("0");
+//     }
+//     return 0;
+// }
