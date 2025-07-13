d94f858e3f66947a5391a0685bfa156f6fb1c658
fev20 <isemy0317@gmail.com>
2025-07-13 20:27:10 +0900

    Add files via upload

---
+++ baekjoon_C_2_7.c
@@ -0,0 +1,27 @@
+int main(){
+    int a, b, c;
+    int d;
+    scanf("%d %d %d", &a, &b, &c);
+    if (a<b){
+        d=a; a=b; b=d;
+    }
+    if (b<c){
+        d=b; b=c; c=d;
+    }
+    if (a<b){
+        d=a; a=b; b=d;
+    }
+    if (a==c){
+        printf("%d", 10000+a*1000);
+    }
+    else if (a==b){
+        printf("%d", 1000+a*100);
+    }
+    else if (c==b){
+        printf("%d", 1000+c*100);
+    }
+    else{
+        printf("%d",a*100);
+    }
+    return 0;
+}
