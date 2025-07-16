c070eb7bb0019488951191c48a5447aae36507b9
fev20 <isemy0317@gmail.com>
2025-07-16 20:23:06 +0900

    Update baekjoon_C_3_12.c

--- baekjoon_C_3/baekjoon_C_3_12.c
+++ baekjoon_C_3/baekjoon_C_3_12.c
@@ -7,3 +7,7 @@ int main(){
     }
     return 0;
 }
+
+// while 문에서 int형 변수에 null이라는 개념은 없음
+// while 문의 논리식 부분에 scanf속 변수의 개수가 참인 개수를 넣어주면 됨
+// ex) while(scanf("%d",a)==1) or while(scanf("%d %d %d %d", &a, &b, &c, &d)==4)
